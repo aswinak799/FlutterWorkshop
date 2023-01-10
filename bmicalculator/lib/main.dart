08e5daa7622298fa4d83c60da789236d834c3e45
ASWIN A K <aswinak799@gmail.com>
2023-01-10 15:32:46 +0530

    Day 2 Bmi calculator

---
+++ bmicalculator/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController height = TextEditingController();
+  TextEditingController weight = TextEditingController();
+  String outPut = "";
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("BMI CALCULATOR"),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(80),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: height,
+                  decoration: InputDecoration(
+                    hintText: "Enter Height in meters",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: weight,
+                  decoration: InputDecoration(
+                      hintText: "Enter Weight in kgs",
+                      border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton.icon(
+                    onPressed: () {
+                      calc();
+                    },
+                    icon: Icon(Icons.calculate),
+                    label: Text("calculate")),
+                Text(
+                  outPut,
+                  style: TextStyle(fontSize: 25, color: Colors.blue),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void calc() {
+    double inputHeight = double.parse(height.text);
+    double inputWeight = double.parse(weight.text);
+
+    // print(inputHeight);
+    setState(() {
+      double heightSq = inputHeight * inputHeight;
+      double result = inputWeight / heightSq;
+      outPut = result.toStringAsFixed(2);
+      print(outPut);
+    });
+  }
+}
